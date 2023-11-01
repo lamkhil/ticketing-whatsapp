94002e0a00ea881b79593f2a2ab398b580d4a9cb
lamkhil <43593728+lamkhil@users.noreply.github.com>
2023-11-01 12:14:54 +0700

    add build up server

---
+++ frontend/build.up/precache-manifest.6296bf25d9300d62196dc799be4aeae8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288207b50b4966c0fdd8c5a3ddcbc863",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d9bd3a9b4aa21b34423",
+    "url": "/static/css/2.38dcfae8.chunk.css"
+  },
+  {
+    "revision": "9d9bd3a9b4aa21b34423",
+    "url": "/static/js/2.cf8a2133.chunk.js"
+  },
+  {
+    "revision": "093bf3f86145069d5ad0de50dd236547",
+    "url": "/static/js/2.cf8a2133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2521ecfbf5bc4eea5cd7",
+    "url": "/static/js/3.90538dde.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.90538dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858262f8cb53d9cdb559",
+    "url": "/static/js/main.2ae42065.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2ae42065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509a257b76adf786af9c",
+    "url": "/static/js/runtime-main.28b6cbb9.js"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.adbccee0.png"
+  }
+]);
